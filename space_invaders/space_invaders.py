11ec178124f64170cb36a935ce176de7c5b6c097
Michael T <tofasttafuri@gmail.com>
2020-10-27 14:29:10 -0400

    Added ship definition to space invaders

--- space_invaders/space_invaders.py
+++ space_invaders/space_invaders.py
@@ -14,4 +14,19 @@ from pygame.locals import *
 
 import pygame
 
-class ship ()
+class Ship(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.image.load('assets/sprites/ship.png').convert_alpha()
+        self.rect = self.surf.get_rect()
+        self.rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 20)
+        self.movey = 0
+        self.movex = 0
+        self.alive = True
+    def handle_keys(self):
+        key = pygame.key.get_pressed()
+    #Checks if sprite collides with enemies
+    def update(self):
+        hit_list = pygame.sprite.spritecollide(self, enemy_list, False)
+        for enemy in hit_list:
+            self.alive = False
