c095490c6f4f70f0c9722f92c7ebb92a4e3f4431
Seth <scp17d@users.noreply.github.com>
2020-11-19 15:10:20 -0500

    fixed some errors, pong ball not bouncing off the wall

--- gui/pygamepong.py
+++ gui/pygamepong.py
@@ -5,60 +5,33 @@ import pygame
 #switched because turtle wasn't very intuitive with start screen
 
 
-paddle1_score = 0
-paddle2_score = 0
-x_speed = 7
-y_ball_speed = 7
-player_speed = 0
-AI_Speed = 6.25
 
-
-def ballmovement():
-	ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15,30,30)
-#	global ball
-	ball.x += 7
-	ball.y += 7
-	
-	######ball bounce#######
-	if ball.top <= 0 or ball.bottom >= screen_height:
-		y_ball_speed *= -1
-	if ball.left <= 0:
-		ball.center = (screen_width/2, screen_height/2)
-		y_ball_speed *= -1
-		x_speed *= -1
-		paddle1_score += 1
-	if ball.right >= screen_width:
-		ball.center = (screen_width/2, screen_height/2)
-		y_ball_speed *= -1
-		x_speed *= -1
-		paddle2_score += 1
-	
-	######ball collision with paddle######
-	if ball.colliderect(paddle1) or ball.colliderect(paddle2):
-		x_speed *= -1
 
 
 def PongGame():
-	global player_speed, paddle1_score, paddle2_score, x_speed, y_ball_speed, AI_speed
 	pygame.init()
 	clock = pygame.time.Clock()
-
 
 	screen_width = 1200
 	screen_height = 800
 	screen = pygame.display.set_mode((screen_width,screen_height))
 	pygame.display.set_caption('PONG GAME')
 
-
-#paddle1 = player paddle2 = opponent AI
 	ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15,30,30)
 	paddle1 = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 	paddle2 = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+	
+	paddle1_score = 0
+	paddle2_score = 0
+	x_speed = 7
+	y_ball_speed = 7
+	player_speed = 0
+	AI_Speed = 6.25
+
 
 	thefont = pygame.font.Font(None, 36)
 
 
-	#pygame draw (r,g,b) 0-255 0 is no color 255 pure color
 
 	bg_color = pygame.Color(0,0,0)
 	paddlecolor = (255, 255, 255)
@@ -68,7 +41,6 @@ def PongGame():
 	x_speed = 7
 	y_ball_speed = 7
 
-#	def ballmovement():
 
 
 	
@@ -79,14 +51,13 @@ def PongGame():
 		start_rect = start_text.get_rect(center = ((screen_width/2), (screen_height/2)))
 		screen.blit(start_text, start_rect)
 
-	######## ball movement function #######
 
 
 	######### Main Loop #########
+
 	running = True
 	start_screen = True
 	while True:
-		#global paddle1_score, paddle2_score, ball, paddle1, paddle2, x_speed, y_ball_speed		
 		while start_screen:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
@@ -96,7 +67,7 @@ def PongGame():
 			screen.fill((0, 0, 0))
 			start()
 			pygame.display.flip()
-		
+				
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
@@ -110,8 +81,8 @@ def PongGame():
 					player_speed -= 7
 				if event.key == pygame.K_UP:
 					player_speed += 7		
-	#######object coloring#######
-
+		#######object coloring#######
+		
 		screen.fill(bg_color)
 
 		pygame.draw.rect(screen,paddlecolor,paddle1)
@@ -120,33 +91,53 @@ def PongGame():
 
 		left_text = thefont.render(f"{paddle2_score}", True, paddlecolor)
 		screen.blit(left_text, (540, 50))
-
 		right_text = thefont.render(f"{paddle1_score}", True, paddlecolor)
 		screen.blit(right_text, (660, 50))
 		pygame.display.update()
 
-		ballmovement()
+		ball.x += 7
+		ball.y += 7
+		
+		######ball bounce#######
+		if ball.top <= 0 or ball.bottom >= screen_height:
+			y_ball_speed *= -1
+		if ball.left <= 0:
+			ball.center = (screen_width/2, screen_height/2)
+			y_ball_speed *= -1
+			x_speed *= -1
+			paddle1_score += 1
+		if ball.right >= screen_width:
+			ball.center = (screen_width/2, screen_height/2)
+			y_ball_speed *= -1
+			x_speed *= -1
+			paddle2_score += 1
+		
+	
+		######ball collision with paddle######
+		if ball.colliderect(paddle1) or ball.colliderect(paddle2):
+			x_speed *= -1	
 
-		#global player_speed
+
+	#global player_speed
 		paddle1.y += player_speed
 		if paddle1.top <= 0:
 			paddle1.top = 0
 		if paddle1.bottom >= screen_height:
-			paddle1.bottom = screen_height
+			paddle1.bottom = screen_height	
 
-	#####AI MOVEMENT#####
-		if paddle2.top < ball.y:
-			paddle2.top += AI_Speed
-		if paddle2.bottom > ball.y:
-			paddle2.bottom -= AI_Speed
-		if paddle2.top <= 0:
-			paddle2.top = 0
-		if paddle2.bottom >= screen_height:
-			paddle2.bottom = screen_height
+		#####AI MOVEMENT#####
+			if paddle2.top < ball.y:
+				paddle2.top += AI_Speed
+			if paddle2.bottom > ball.y:
+				paddle2.bottom -= AI_Speed
+			if paddle2.top <= 0:
+				paddle2.top = 0
+			if paddle2.bottom >= screen_height:
+				paddle2.bottom = screen_height
 		
 		
-		pygame.display.flip()
-		clock.tick(50)
+			pygame.display.flip()
+			clock.tick(50)
 
 
 PongGame()
