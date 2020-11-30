0dad823722741ea2b6fd8bcd4b860a9a755140d7
Joe Bellissimo <joeybellissimo@gmail.com>
2020-11-30 15:12:58 -0500

    added sounds of death and growth, added music

--- gui/snake.py
+++ gui/snake.py
@@ -3,8 +3,11 @@ import sys
 import time
 import random
 import pygame.locals
+from os import environ
 from pygame.locals import *
-#environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
 def Snake():
     FPS = 15
@@ -17,15 +20,18 @@ def Snake():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
     surface = pygame.Surface(screen.get_size())
     surface = surface.convert()
-    black = (0,0,0)
-    white = (255,255,255)
+    black = (0, 0, 0)
+    white = (255, 255, 255)
     clock = pygame.time.Clock()
     pygame.key.set_repeat(1, 40)
     GRIDSIZE = 10
     GRID_WIDTH = SCREEN_WIDTH / GRIDSIZE
     GRID_HEIGHT = SCREEN_HEIGHT / GRIDSIZE
-    #runningSound = pygame.mixer.sound("assets/sounds/sample.wav")
-    #deathSound = pygame.mixer.sound("assets/sounds/sample2.wav")
+    #runningSound = pygame.mixer.sound("[ONTIVA.COM] 8-Bit RPG Music - Dance-off _ Original Composition-64k.wav")
+    pygame.mixer.music.load("assets/sounds/[ONTIVA.COM] 8-Bit RPG Music - Dance-off _ Original Composition-64k.wav")
+    pygame.mixer.music.play(-1)
+    deathSound = pygame.mixer.Sound("assets/sounds/deathsound.wav")
+    eatSound = pygame.mixer.Sound("assets/sound/levelup.wav")
     UP = (0, -1)
     DOWN = (0, 1)
     LEFT = (-1, 0)
@@ -33,14 +39,13 @@ def Snake():
 
     screen.blit(surface, (0, 0))
 
-
     def draw_box(surf, color, pos):
         r = pygame.Rect((pos[0], pos[1]), (GRIDSIZE, GRIDSIZE))
         pygame.draw.rect(surf, color, r)
 
     def startScreen():
         font = pygame.font.Font(None, 36)
-        text = font.render("Press Space to Start", True, (255,255,255))
+        text = font.render("Press Space to Start", True, (255, 255, 255))
         window = font.render("Press Space to Start (Q or ESC to Quit)", True, (0, 0, 0))
         tRect = text.get_rect(center=((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2) - 110))
         oect = window.get_rect(center=((SCREEN_WIDTH / 2) + 2, (SCREEN_HEIGHT / 2) - 108))
@@ -49,7 +54,7 @@ def Snake():
 
     def loseScreen():
         font = pygame.font.Font(None, 36)
-        text = font.render("Press Space to Continue", True, (255,255,255))
+        text = font.render("Press Space to Continue", True, (255, 255, 255))
         window = font.render("Press Space to Continue (Q or ESC to Quit)", True, (0, 0, 0))
         tRect = text.get_rect(center=((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2) - 110))
         oect = window.get_rect(center=((SCREEN_WIDTH / 2) + 2, (SCREEN_HEIGHT / 2) - 108))
@@ -68,7 +73,9 @@ def Snake():
             self.length = 1
             self.positions = [((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))]
             self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
-
+            pygame.mixer.Sound.play(deathSound)
+            pygame.mixer.music.stop()
+            print("Death to the snake")
 
         def point(self, pt):
             if self.length > 2 and (pt[0] * -1, pt[1] * -1) == self.direction:
@@ -81,7 +88,7 @@ def Snake():
 
         def move(self):
             cur = self.positions[0]
-            x,y = self.direction
+            x, y = self.direction
             new = (((cur[0] + (x * GRIDSIZE)) % SCREEN_WIDTH), (cur[1] + (y * GRIDSIZE)) % SCREEN_HEIGHT)
             if len(self.positions) > 2 and new in self.positions[2:]:
                 self.lose()
@@ -94,7 +101,6 @@ def Snake():
             for p in self.positions:
                 draw_box(surf, self.color, p)
 
-
     class Apple(object):
         def __init__(self):
             self.position = (0, 0)
@@ -105,13 +111,16 @@ def Snake():
             draw_box(surf, self.color, self.position)
 
         def randomize(self):
-            self.position = (random.randint(0, GRID_WIDTH - 1) * GRIDSIZE, random.randint(0, GRID_HEIGHT - 1) * GRIDSIZE)
-
+            self.position = (
+            random.randint(0, GRID_WIDTH - 1) * GRIDSIZE, random.randint(0, GRID_HEIGHT - 1) * GRIDSIZE)
 
     def check_eat(snake, apple):
         if snake.get_head_position() == apple.position:
             snake.length = snake.length + 1
             apple.randomize()
+            pygame.mixer.Sound.play(eatSound)
+            pygame.mixer.music.stop()
+            print("Snake has eaten an apple")
 
     player = Snake()
     goal = Apple()
@@ -135,7 +144,7 @@ def Snake():
                             highest_score = player.length
                         return highest_score
                 pygame.display.update()
-            
+
             font = pygame.font.Font(None, 30)
             text = font.render("Press Space to Start", True, white, black)
             tRect = text.get_rect(center=((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2) - 110))
@@ -166,6 +175,9 @@ def Snake():
                             lost = True
                             break
                     if event.type == QUIT or (event.type == KEYDOWN and (event.key == K_ESCAPE or event.key == K_q)):
+                        pygame.mixer.Sound.play(deathSound)
+                        pygame.mixer.music.stop()
+                        print("User has quit :(")
                         pygame.quit()
                         if player.length > highest_score:
                             highest_score = player.length
@@ -183,7 +195,7 @@ def Snake():
             player.draw(surface)
             goal.draw(surface)
             player.move()
-    
+
             font = pygame.font.Font(None, 36)
             text = font.render(str(player.length), 1, (white))
             textpos = text.get_rect()
@@ -193,7 +205,7 @@ def Snake():
             pygame.display.flip()
             pygame.display.update()
             fpsClock.tick(FPS)
-        
+
         while lost:
             surface.fill(black)
             pygame.display.update()
@@ -207,7 +219,7 @@ def Snake():
                         if player.length > highest_score:
                             highest_score = player.length
                         return highest_score
-                
+
             font = pygame.font.Font(None, 30)
             text = font.render("You lose. Press Space to Continue", 1, white, black)
             tRect = text.get_rect(center=((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2) - 110))
